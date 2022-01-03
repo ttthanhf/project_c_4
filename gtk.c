c1d530826e49e3fcb77221ffa31d90641989cab0
ThanhF <trthanh179@gmail.com>
2022-01-03 14:28:46 +0700

    update more gtk and add css

--- gtk.c
+++ gtk.c
@@ -1,39 +1,55 @@
 #include <gtk/gtk.h> // khai báo thứ viện gtk
 #include <stdio.h> //khai báo thư viện c
 
-static void on_click1(GtkWidget *widget, gpointer data) {
-  g_print("Bạn đã bị thiểu năng\n");
+static void load_css() {
+  GtkCssProvider *cssProvider = gtk_css_provider_new(); //khai báo css provider mới
+  gtk_css_provider_load_from_path(cssProvider, "theme.css", NULL); // đọc file css
+  gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),GTK_STYLE_PROVIDER(cssProvider),GTK_STYLE_PROVIDER_PRIORITY_USER); 
 }
 
 int main(int argc, char *argv[]) {
 
   gtk_init(&argc, &argv); // khai báo  
 
-  GtkWidget *window, *fixed, *notebook;
-  GtkWidget *button1, *button_exit;
+  //add biến
+  GtkWidget *window, *fixed;
+  GtkWidget *button_exit, *button_login, *button_register;
   
-
+  //set biến
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL); //cho app ở quyền cao nhất
-  fixed = gtk_fixed_new();
+  fixed = gtk_fixed_new(); // khai báo biến fixed mới
 
+  //
   gtk_window_set_title(GTK_WINDOW(window),"Lịch thiểu năng"); // title cho app
   gtk_window_set_position(GTK_WINDOW(window),GTK_WIN_POS_CENTER); // canh app khi mở sẽ ở giữa màn hình
-  gtk_window_set_default_size(GTK_WINDOW(window),1280,820); // kích thước app
+  gtk_window_set_default_size(GTK_WINDOW(window),1280,820); // kích thước app mặc định
   gtk_window_set_resizable(GTK_WINDOW(window),FALSE); // false sẽ làm cho app ko zoom out or in được
+  gtk_container_set_border_width(GTK_CONTAINER(window),10);// tạo khoảng cách giữa độ rộng tối đa của app và các thành phần bên trong
 
-  button1 = gtk_button_new_with_label("Nút thiểu năng"); // tạo button
-  button_exit = gtk_button_new_with_label("Thoát");
+  //tạo button
+  button_exit = gtk_button_new_with_label("Exit");
+  button_login = gtk_button_new_with_label("Login");
+  button_register = gtk_button_new_with_label("Register");
 
-  gtk_fixed_put(GTK_FIXED(fixed), button1, 50, 50); // set vị trí cho button
-  gtk_fixed_put(GTK_FIXED(fixed), button_exit, 50, 720);
+  //tạo khả năng fixed cho từng thành phần và mặc định vị trí
+  gtk_fixed_put(GTK_FIXED(fixed), button_exit, 1000, 680); 
+  gtk_fixed_put(GTK_FIXED(fixed), button_login, 1000, 100);
+  gtk_fixed_put(GTK_FIXED(fixed), button_register, 1000, 150);
 
-  gtk_container_add(GTK_CONTAINER(window),fixed);
+  //set biến thành id name để css có thể nhận dạng
+  gtk_widget_set_name(button_exit,"button_exit"); 
+  gtk_widget_set_name(button_login,"button_login"); 
+  gtk_widget_set_name(button_register,"button_register"); 
+
+  gtk_container_add(GTK_CONTAINER(window),fixed); // thêm phần window chứa fixed để hiển thị toàn bộ thành phần được put fixed
 
   g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL); // tắt app sẽ tắt mọi thứ chạy chung bao gồm console và log
   g_signal_connect(button_exit,"clicked",G_CALLBACK(gtk_main_quit),NULL);
-  g_signal_connect(button1,"clicked",G_CALLBACK(on_click1),NULL);
 
-  gtk_widget_show_all(window);
+  gtk_widget_show_all(window); // hiển thị 
+
+  load_css();// gọi hàm load_css
+
   gtk_main(); //mainLoop
 
   return 0;
